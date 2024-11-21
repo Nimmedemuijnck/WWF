871dcc80d360aba83701ae8f798c85b953cacd67
Nimmedemuijnck <145469692+Nimmedemuijnck@users.noreply.github.com>
2024-11-21 14:09:01 +0100

    bh

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+var toonMenu = document.querySelector("nav button"); 
+var hetMenu = document.querySelector("nav"); 
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+toonMenu.onclick = openMenu;
+
+// stap 3: voeg in de functie een class toe aan de nav
+function openMenu() {
+hetMenu.classList.toggle("toonMenu"); 
+}
